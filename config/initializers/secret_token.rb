fa9adc2f346e994f515b89c59998a50ac63e576c
Marcela Garizao <marcela.garizao@koombea.com>
2011-09-26 16:01:31 -0500

    adding rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soluvet::Application.config.secret_token = '9e3dc1f5d2af26e6b7802a54f5f1cb115c3171e5afc4c58affdc72bd7a11a9aa6a3a271b7b383bb4a2e2087ef7610575dca2ac608b5431a80843eed5c28607d8'
